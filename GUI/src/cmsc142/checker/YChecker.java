b6a71556e75e1d118588ecf33ee8c04de14b711e
Joseph Gabriel Adaoag <jgj.adaoag@gmail.com>
2016-11-25 13:54:12 +0800

    Fix YChecker

--- GUI/src/cmsc142/checker/YChecker.java
+++ GUI/src/cmsc142/checker/YChecker.java
@@ -1,30 +1,46 @@
 package cmsc142.checker;
 
-import cmsc142.Puzzle;
 import cmsc142.Point;
 
 public class YChecker extends CheckerStrategy {
+    private boolean checkLeft(int candidate) {
+		for(int i = 0, j = 0; i < (size*size)/2; i++, j++){
+			if(candidate == this.puzzle[i][j])
+				return false;
+		}
+		return true;
+	}
+
+	private boolean checkRight(int candidate) {
+		for(int i = 0, j = size*size-1; i < (size*size)/2; i++, j--){
+			if(candidate == this.puzzle[i][j])
+				return false;
+		}
+		return true;
+	}
+
+	private boolean checkBottom(int candidate) {
+		for(int i = (size*size)/2, j = (size*size)/2; i < (size*size); i++) {
+			if(candidate == this.puzzle[i][j])
+				return false;
+		}
+		return true;
+	}
+
 	public boolean candidateCheck(int candidate, Point p) {
 		if(size % 2 == 0) return false;
 
-		if((p.x == p.y || (p.x+p.y) == (size*size) - 1) && p.x <= (size*size)/2) {
-			for(int i = 0, j = 0; i < (size*size)/2; i++, j++){
-				if(candidate == this.puzzle[i][j])
-					return false;
+		if (p.y < size*size/2) {
+			if (p.x == p.y) {
+				return checkRight(candidate) && checkBottom(candidate);
+			} else if (p.x + p.y == size * size - 1) {
+				return checkLeft(candidate) && checkBottom(candidate);
 			}
-			for(int i = (size*size)/2, j = (size*size)/2; i < (size*size); i++) {
-				if(candidate == this.puzzle[i][j])
-					return false;
-			}
-			for(int i = ((size*size)/2)-1, j = ((size*size)/2)+1; i >= 0; i--, j++){
-				if(candidate == this.puzzle[i][j])
-					return false;
-			}
-			for(int i = (size*size)/2, j = (size*size)/2; i < (size*size); i++) {
-				if(candidate == this.puzzle[i][j])
-					return false;
-			}
+		} else if (p.x == size*size/2) {
+			return checkLeft(candidate) && checkRight(candidate) &&
+					checkBottom(candidate);
 		}
+
 		return true;
 	}
 
